93b95f09cba46eb280211ad0d0b6345d2507699a
JoseADAW <1898288@alu.murciaeduca.es>
2022-09-27 21:18:42 +0200

    form

--- app/inicio.php
+++ app/inicio.php
@@ -4,5 +4,6 @@ ini_set('display_errors', 1);
 
 //carga las clases iniciales
 require_once ('libs/Mysqldb.php');
+require_once ('libs/Controller.php');
 require_once('libs/Application.php');
 ?>
--- app/libs/Application.php
+++ app/libs/Application.php
@@ -11,38 +11,52 @@
 
      function __construct()
      {
-         print 'Prueba a ver si va';
-    $db = Mysqldb::getInstance()->getDatabase();
 
-    $url = $this->separarURL();
+    $this->separarURL();
 
-    if (! $this->urlController){
-        require_once '../app/controllers/loginController.php';
-        $page = new LoginController();
-        $page->index();
-    } elseif (file_exists('../app/controllers/' . ucfirst($this->urlController) . 'Controller.php')) {
-        $controller = ucfirst($this->urlController ) . 'Controller';
-        require_once '../app/controllers/' . $controller . '.php';
-        $this->urlController = new $controller;
-        $this->urlController->index();
-    }
+         if ( ! $this->urlController ) {
+             require_once '../app/controllers/loginController.php';
+             $page = new LoginController();
+             $page->index();
+         } elseif (file_exists('../app/controllers/' . ucfirst($this->urlController) . 'Controller.php')) {
+             $controller = ucfirst($this->urlController) . 'Controller';
+             require_once '../app/controllers/' . $controller . '.php';
+             $this->urlController = new $controller;
 
+             if (method_exists($this->urlController, $this->urlAction) &&
+                 is_callable(array($this->urlController, $this->urlAction))) {
+                 if ( ! empty($this->urlParams) ) {
+                     call_user_func_array(array($this->urlController, $this->urlAction), $this->urlParams);
+                 } else {
+                     $this->urlController->{$this->urlAction}();
+                 }
+             } else {
+                 if (strlen($this->urlAction) == 0) {
+                     $this->urlController->index();
+                 } else {
+                     header('HTTP/1.0 404 Not Found');
 
-
-    //separar url (ver si funciona) var_dump($url);
+                     // Tratamos el error producido cuando creemos el controlador de Error
+                 }
+             }
+         } else {
+             require_once '../app/controllers/LoginController.php';
+             $page = new LoginController();
+             $page->index();
+         }
      }
 
-     public function separarURL()
+     public function separarUrl()
      {
-         if ($_SERVER['REQUEST_URI'] != '/'){
+         if ($_SERVER['REQUEST_URI'] != '/') {
              $url = trim($_SERVER['REQUEST_URI'], '/');
              $url = filter_var($url, FILTER_SANITIZE_URL);
-             $url = explode('/' , $url);
+             $url = explode('/', $url);
 
              $this->urlController = $url[0] ?? null;
              $this->urlAction = $url[1] ?? null;
 
-             unset($url[0],$url[1]);
+             unset($url[0], $url[1]);
 
              $this->urlParams = array_values($url);
          }
