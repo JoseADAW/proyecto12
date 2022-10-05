cb99348d9c68f10978558002728db6eb4deb1873
JoseADAW <1898288@alu.murciaeduca.es>
2022-10-05 20:18:22 +0200

    no funciona

--- app/controllers/AdminUserController.php
+++ app/controllers/AdminUserController.php
@@ -23,14 +23,73 @@ class AdminUserController extends Controller
 
     public function create()
     {
-        $data = [
-            'titulo' => 'Administración de Usuarios - Alta',
-            'menu' => false,
-            'admin' => true,
-            'data' => [],
-        ];
 
-        $this->view('admin/users/create', $data);
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+            $erros = [];
+            $name = $_POST['name'] ?? '';
+            $email = $_POST['email'] ?? '';
+            $password1 = $_POST['password1'] ?? '';
+            $password2 = $_POST['password2'] ?? '';
+
+            $dataForm = [
+                'name' => $name,
+                'email' => $email,
+                'password' => $password1,
+            ];
+
+            if (empty($name)){
+                array_push($erros, 'El nombre el requerido');
+            }
+
+            if (empty($email)){
+                array_push($erros, 'El email el requerido');
+            }
+
+            if (empty($password1)){
+                array_push($erros, 'La contraseña es requerida');
+            }
+
+            if (empty($password2)){
+                array_push($erros, 'La verificacion de clave es requerida');
+            }
+
+            if ($password1 != $password2){
+                array_push($erros, 'Las claves on coinciden');
+            }
+
+            if (! $erros){
+
+                if ($this->model->createAdminUser($dataForm)){
+
+                }else{
+
+                }
+
+            }else{
+                $data = [
+                    'titulo' => 'Administración de Usuarios - Alta',
+                    'menu' => false,
+                    'admin' => true,
+                    'erros' => $erros,
+                    'data' => $dataForm,
+                ];
+
+                $this->view('admin/users/create', $data);
+            }
+
+
+        }else{
+            $data = [
+                'titulo' => 'Administración de Usuarios - Alta',
+                'menu' => false,
+                'admin' => true,
+                'data' => [],
+            ];
+
+            $this->view('admin/users/create', $data);
+        }
+
     }
 
     public function update()
--- app/models/AdminUser.php
+++ app/models/AdminUser.php
@@ -8,4 +8,40 @@ class AdminUser
     {
         $this->db = Mysqldb::getInstance()->getDatabase();
     }
+
+    public function createAdminUser($data)
+    {
+        $response = false;
+
+        if (! $this->existsEmail($data['email'])){
+
+            $passwords = hash_hmac('sha512', $data['password'], ENCRIPTKEY);
+
+            $sql = 'INSERT INTO admins(name,email,password,status,deleted,login_at,created_at,updated_ad,daleted_at) VALUES (:name,:email,:password,:status,:deleted,:login_at,:created_at,:updated_ad,:daleted_at)'
+
+                $params = [
+                    ':name' => $data['name'],
+                    ':email' => $data['email'],
+                    ':password' => $data['password'],
+                    ':status' => $data['status'],
+                    ':deleted' => $data['deleted'],
+                    ':login_at' => $data['login_at'],
+                    ':created_at' => $data['created_at'],
+                    ':updated_ad' => $data['updated_ad'],
+                    ':daleted_at' => $data['daleted_at'],
+                ];
+
+            $query = $this->db->prepare($sql);
+            $response = $query->execute($params);
+        }
+        return $response;
+    }
+
+    public function existsEmail($email)
+    {
+        $sql = 'SELECT * FROM admins WHERE email=:email';
+        $query = $this->db->prepare($sql);
+        $query->bindParam('email', $email, PDO::PARAM_STR);
+        $query->execute();
+    }
 }
