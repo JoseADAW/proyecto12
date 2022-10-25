5a26bcd072f48188f62c095b0e84e4230c67e95c
JoseADAW <1898288@alu.murciaeduca.es>
2022-10-25 19:57:20 +0200

    funciona boton recalcular

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -9,7 +9,7 @@ class CartController extends Controller
         $this->model = $this->model('Cart');
     }
 
-    public function index()
+    public function index($errors = [])
     {
         $session = new Session();
 
@@ -20,8 +20,9 @@ class CartController extends Controller
             $data = [
                 'titulo' => 'Carrito',
                 'menu' => true,
-            'user_id' => $user_id,
-            'data' => $cart,
+                'user_id' => $user_id,
+                'data' => $cart,
+                'errors' => $errors,
             ];
 
             $this->view('carts/index', $data);
@@ -36,10 +37,29 @@ class CartController extends Controller
         $errors =[];
 
         if ($this->model->verifyProduct($product_id, $user_id) == false){
-        if ($this->model->addProduct($product_id, $user_id) == false){
+        if ($this->model->addProduct($product_id, $user_id) == false){            array_push($errors,'Error al insertar el producto en el carrito');
+
             array_push($errors,'Error al insertar el producto en el carrito');
         }
         }
-        $this->index();
+        $this->index($errors);
+    }
+
+    public function update()
+    {
+        if (isset($_POST['rows']) && isset($_POST['user_id'])){
+            $errors =[];
+            $rows = $_POST['rows'];
+            $user_id = $_POST['user_id'];
+
+            for ($i = 0; $i < $rows; $i++){
+                $product_id = $_POST['i'.$i];
+                $quantity = $_POST['c'.$i];
+                if (! $this->model->update($user_id,$product_id,$quantity)){
+                    array_push($errors,'Error al actualizar el producto');
+                }
+            }
+            $this->index($errors);
+        }
     }
 }
